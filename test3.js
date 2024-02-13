7378dbb40322d61def14458c69a2128b497f4e4c
Ujjawal Joshi <ujjawaljoshi07@gmail.com>
2024-02-13 21:36:51 +0530

    addded test3 and test4

---
+++ test3.js
@@ -0,0 +1,5 @@
+// Create an array named colors that contains five different names of colors as strings.
+const colors = ['Red', 'Blue', 'Green', 'Yellow', 'Orange']
+
+// Access the first color in the array and print it to the console using console.log()
+console.log(colors[0])
