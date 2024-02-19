9178efb44e27ec1dc17e29f42a892bf55aaffd0f
Ujjawal Joshi <ujjawaljoshi07@gmail.com>
2024-02-20 00:37:09 +0530

    added test7

---
+++ test7.js
@@ -0,0 +1,22 @@
+greet('John')
+
+var greet = (name) => {
+    console.log('Good Day', name)
+}
+
+function greet(name) {
+    console.log('Hello', name)
+}
+
+greet('John')
+
+
+
+/* Solution :
+    Phase 1(Memory creation Phase): Here the whole Execution context will be created 
+    where first the var 'greet' will be assigned undefined and later the whole function
+    as the second greet() is the function statement.
+    
+    Phase 2(Code Execution Phase): The first function call will result to the one in the function statement one.The second line is executed and now the greet var value is changed to the new one and hence 
+    in the second function call it will result to the var greet one.
+*/
